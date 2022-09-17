c2160a335c25adcb0407b77812b76bc02d55c349
Al7awy <ibrahimfares511@gmail.com>
2022-09-17 22:41:29 +0200

    test route

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('alaa',function (){
+  return 'Alaa';
+});
 
 Route::get('/home', function () {
     return redirect()->route('admin.general');
